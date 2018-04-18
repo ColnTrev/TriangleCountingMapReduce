04b22b03677560843f8be8fdda1059e310d7606b
Collin Gordon <colntrev@gmail.com>
2018-04-18 14:01:00 -0700

    added in duplicate preventing logic.

--- src/GraphEdgeReduce.java
+++ src/GraphEdgeReduce.java
@@ -2,36 +2,42 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.mapreduce.Reducer;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 
 /**
  * Created by colntrev on 2/18/18.
  */
 public class GraphEdgeReduce extends Reducer<IntWritable,IntWritable,EdgePair,IntWritable> {
-    EdgePair pair = new EdgePair();
-    IntWritable connector = new IntWritable();
     @Override
     protected void reduce(IntWritable node, Iterable<IntWritable> edges, Context context) throws IOException,InterruptedException {
-
+        Map<EdgePair,List<IntWritable>> duplicates = new HashMap<>();
         List<Integer> cache = new ArrayList<>(); // add values from iterable here since iterable passes once
         //write edges in K,V pairs with a null connector
         // example [2,1],(-)
-        connector.set(0);
         for(IntWritable edge : edges){
+            IntWritable connector = new IntWritable(0);
             cache.add(edge.get());
-            pair.setEdge(node.get(), edge.get());
-            context.write(pair,connector);
+            EdgePair pair = new EdgePair(node.get(), edge.get());
+            if(!duplicates.containsKey(pair)){
+                duplicates.put(pair, new ArrayList<>());
+            }
+            if(!duplicates.get(pair).contains(connector)) {
+                duplicates.get(pair).add(connector);
+                context.write(pair, connector);
+            }
         }
         Collections.sort(cache);
         //write edges with V,V pairs with their K connection
         // example [1,3], [2]
+
         for(int i = 0; i < cache.size(); i++){
             for(int j = i + 1; j < cache.size(); j++){
-                    pair.setEdge(cache.get(i), cache.get(j));
+                EdgePair pair = new EdgePair(cache.get(i), cache.get(j));
+                if(!duplicates.get(pair).contains(node)) {
+                    duplicates.get(pair).add(node);
                     context.write(pair, node);
+                }
             }
         }
     }
